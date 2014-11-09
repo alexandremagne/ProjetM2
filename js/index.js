a38b6ec8c685c8671967eb2e4f53a5bdc8bb8ce7
RomainCucu <romain.cunault@gmail.com>
2014-11-10 00:55:17 +0100

    2 fonctions pour remplir l'objet data à transmettre au router

--- js/index.js
+++ js/index.js
@@ -1,28 +1,46 @@
 
 
 var index = {};
+var data = {};//objet ) transmettre au routeur
 
 index.start = function () {
 	index.btn_check_information_for_loan_demand_();//action du bouton check loan demand
-	index.btn_check_login_formular_();
+	index.btn_check_login_formular_();//action du bouton login
+	
+
 };
 
 index.btn_check_information_for_loan_demand_ = function(){
 	//ce quil se passe quand on appuie sur le bouton check
 	$( "#check_formulaire_" ).submit(function( event ) {
-	data={ac:"check_loan_info_action_",ar:0};
+		index.fill_data_loan_demand_();
 	 index.post(data, index.callback);
 	 event.preventDefault();//à laisser
 	});
 };
 
 index.btn_check_login_formular_ = function(){
-	//ce quil se passe quand on appuie sur le bouton check
+	//ce quil se passe quand on appuie sur le bouton login
 	$( "#log_in_formular_" ).submit(function( event ) {
-	data={ac:"check_login_process_"};
+	 index.fill_data_login();//action pour remplir data
 	 index.post(data, index.callback);
 	 event.preventDefault();//à laisser
 	});
+};
+
+index.fill_data_login = function(){
+	//pour remplir l'objet data avec le username et password et l'action à réaliser
+	data.ac = "check_login_process_";
+	data.userName = document.getElementById('input_username_').value;
+	data.password = document.getElementById('input_password_').value;
+};
+
+index.fill_data_loan_demand_ = function(){
+	data.ac="check_loan_info_action_";
+	data.input_borrowed_capital_ = document.getElementById('input_borrowed_capital_').value;
+	data.input_age_of_demander_ = document.getElementById('input_age_of_demander_').value;
+	data.input_monthly_incomes_ = document.getElementById('input_monthly_incomes_').value;
+	data.input_duration_loan_in_years_ = document.getElementById('input_duration_loan_in_years_').value;
 };
 
 //fonction post
