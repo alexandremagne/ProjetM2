cc50531459800c1089b69f5a22cd057c59bd1cb4
RomainCucu <romain.cunault@gmail.com>
2014-11-10 00:59:01 +0100

    ajout de commentaire dans db.js pour voir ce quil faut faire un peu

--- db.js
+++ db.js
@@ -1,6 +1,15 @@
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
 
+
+/*
+####################################
+####################################
+FONCITION POUR AFFICHER TOUTE LA BASE
+RETOURNE JE NE SAIS PLUS QUOI DONC A REMPLI
+####################################
+####################################
+*/
 exports.afficher_toute_la_base = function(){
 MongoClient.connect('mongodb://alexandre:magne@dogen.mongohq.com:10036/ProjetEsme', function(err, db) {
     if(err) throw err;//si erreur de connections
@@ -16,3 +25,30 @@ MongoClient.connect('mongodb://alexandre:magne@dogen.mongohq.com:10036/ProjetEsm
 
 });
 };
+
+/*
+####################################
+####################################
+FONCTION POUR AJOUTER LES INFOS DE DEMANDE DE PRET
+A FAIRE
+####################################
+####################################
+*/
+
+/*
+####################################
+####################################
+FONCTION POUR AJOUTER LES COOKIES DANS UNE **AUTRE COLLECTION** QUE CELLE OU LON MET LES INFOS DE DEMANDE DE PRET
+A FAIRE
+####################################
+####################################
+*/
+
+/*
+####################################
+####################################
+FONCTION POUR RECUPERER LES COOKIES DANS LA COLLECTION APPARTENANT A CELLE DES COOKIES
+A FAIRE
+####################################
+####################################
+*/
