3e21151c0dc83bb008c9d93164830072f61f3a1f
RomainCucu <romain.cunault@gmail.com>
2014-11-12 19:29:48 +0100

    ajout du lien pour aller au menu principal

--- router.js
+++ router.js
@@ -80,7 +80,7 @@ check_user:
 			console.log("cookie ok");
 			this.read_file();
 		}else{
-			this.resp.end("<p>non connecté</p>");
+			this.resp.end('<p>non connect&eacute</p><A HREF="../../index.html">Cliquer pour aller au menu principal</A>');
 		}
 	},
 
