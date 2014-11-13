12c7b43fc2206ae504b7bcd7a3e3e8c32624df5d
RomainCucu <romain.cunault@gmail.com>
2014-11-13 23:58:30 +0100

    t vraiment relou je dois surpprimer tes console log

--- db.js
+++ db.js
@@ -84,16 +84,7 @@ exports.valid_cookie = function(c,obj,fct){
 				MongoClient.connect('mongodb://alexandre:magne@dogen.mongohq.com:10036/ProjetEsme', function(err, db) {
 				    if(err) throw err;	
 					var collection = db.collection('users');//pour aller choper le cookie dans la db
-					console.log("Valeur de c avant le split : " + c);
 					c = c.split("cookieName=");//car c ="GA=iyiuyeuiyizeu ; cookieName=rom19282839" par excemple donc on eneleve le cookieName
-
-					// collection.find({"cookie.value": c[1]}).toArray(function(err, results) {
-
-					console.log("Valeur de c :" + c);
-					console.log("Valeur de c[1] :" + c[1]);
-					//var cookie_name=c[1].substr(0, 11); // on ne récupère que la valeur du cookie_name
-					//console.log(cookie_name);
-					//collection.find({"cookie.value": cookie_name}).toArray(function(err, results) {
 collection.find({"cookie.value": c[1]}).toArray(function(err, results) {
 					 if (err){
 					 	console.log(err);
