e6dac925235475610a71804e438fa79cea715aff
RomainCucu <romain.cunault@gmail.com>
2014-11-13 13:28:17 +0100

    port server pour hebergement

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var util = require("util");
 
 var server = {}; //Server object. This object is use to stock everything owned by the server.
 server.r = require("./router.js");
-server.port = 5000;
+//server.port = 5000;
 server.address = "0.0.0.0";
 /**
 * This method is called each times a request arrives on the server * @param req (Object) request object for this request
@@ -16,5 +16,5 @@ server.address = "0.0.0.0";
 server.receive_request = function (req, resp) {
 	server.r.router(req, resp);
 };
-http.createServer(server.receive_request).listen(server.port, "0.0.0.0");
+http.createServer(server.receive_request).listen((process.env.PORT || 5000), "0.0.0.0");
 util.log("INFO - Server started, listening " + server.address + ":" + server.port);
