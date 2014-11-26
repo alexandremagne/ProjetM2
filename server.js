4435cf6379931e8a42720bca90bcccd3e9fabd7d
romaincucu <romain.cunault@gmail.com>
2014-11-26 17:32:26 +0100

    k

--- server.js
+++ server.js
@@ -18,3 +18,28 @@ server.receive_request = function (req, resp) {
 };
 http.createServer(server.receive_request).listen(server.port , "0.0.0.0");
 util.log("INFO - Server started, listening " + server.address + ":" + server.port);
+
+
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: 'https://sleepy-gorge-9408.herokuapp.com/',
+            port: 80,
+            path: '/'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                    // optional logging... disable after it's working
+                    console.log("HEROKU RESPONSE: " + chunk);
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
