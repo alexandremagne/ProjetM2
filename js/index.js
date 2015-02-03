ab50d779637251de6d5b2f97959f577ee33eb38c
alexandremagne <alexandre.magne@hotmail.fr>
2015-02-04 00:29:38 +0100

    regex faites pour les 3 champs de la calculette

--- js/index.js
+++ js/index.js
@@ -17,6 +17,9 @@ index.start = function () {
 	index.btn_login_formulaire_();//formulaire du login
 	index.btn_register_formulaire_();//formulaire du register
 	index.btn_button_calculator_();//formulaire du calculator
+
+	index.regozoo();
+
 };
 
 index.btn_register_formulaire_ = function(){
@@ -79,9 +82,9 @@ index.btn_button_calculator_ = function(){
 index.data_calculator = function(){
 	//pour remplir l'objet data avec le montant, year, rate et l'action à réaliser pour le router ???????????????
 	//data.ac = "calculator_"; // action a traité pour le routeur
-	data.amount = document.getElementById('input_loan_amount_').value;
-	data.duration = document.getElementById('input_loan_duration_').value;
-	data.rate = document.getElementById('input_interest_rate_').value;
+	data.amount = parseInt(document.getElementById('input_loan_amount_').value.replace(/ /g,""));
+	data.duration = parseInt(document.getElementById('input_loan_duration_').value);
+	data.rate = parseFloat(document.getElementById('input_interest_rate_').value);
 	console.log("Vos trois valeurs: " + data.amount + " " + data.duration + " " + data.rate);
 	index.calculation_of_monthly_payments(data.amount, data.duration, data.rate);
 };
@@ -91,9 +94,131 @@ index.calculation_of_monthly_payments=function(amount, duration, rate){
 	var result= (amount / ( (1- (Math.pow( (1+(rate/1200)), ((-1)*(duration*12)) )) ) / (rate/1200)));
 	credit_cost=((result*12*duration)-amount);
 	//<h3>Example heading <span class="label label-default">New</span></h3>
-	var string_result="<h3>Your monthly repayment: <span class=\"label label-default\">"+ Math.round(result*100)/100 +"€</span></h3><h3>Total cost of the credit: <span class=\"label label-default\">"+ Math.round(credit_cost*100)/100+" €</span></h3>";
+	var string_result="<h3>Your monthly repayment: <span class=\"label label-success\">"+ Math.round(result*100)/100 +"€</span></h3><h3>Total cost of the credit: <span class=\"label label-success\">"+ Math.round(credit_cost*100)/100+" €</span></h3>";
 	document.getElementById("result_calculator").innerHTML=string_result;
 	console.log(result);
+};
+
+document.getElementById("input_loan_amount_").onkeyup=function(){
+		index.regexnumber();
+};
+
+document.getElementById("input_loan_duration_").onkeyup=function(){
+		index.regexnumber2();
+};
+
+
+document.getElementById("input_loan_amount_").onblur=function(){
+	console.log("onblur");
+	var nombre_separe=index.lisibilite_nombre(document.getElementById("input_loan_amount_").value);
+	document.getElementById("input_loan_amount_").value=nombre_separe;
+};
+	
+
+index.lisibilite_nombre=function (nbr) {
+	console.log("visibilit");
+		var nombre = ''+nbr;
+		var retour = '';
+		var count=0;
+		for(var i=nombre.length-1 ; i>=0 ; i--)
+		{
+			if(count!=0 && count % 3 == 0)
+				retour = nombre[i]+' '+retour ;
+			else
+				retour = nombre[i]+retour ;
+			count++;
+		}
+		//alert('nb : '+nbr+' => '+retour);
+		return retour;
+};
+
+index.regexnumber=function(){
+	var exp = new RegExp("[^0-9]","gi");
+	document.getElementById("input_loan_amount_").value=document.getElementById("input_loan_amount_").value.replace(exp,"");
+};
+
+index.regexnumber2=function(){
+	var exp = new RegExp("[^0-9]","gi");
+	document.getElementById("input_loan_duration_").value=document.getElementById("input_loan_duration_").value.replace(exp,"");
+	var duration= parseInt(document.getElementById("input_loan_duration_").value);
+	if(duration>25){
+		document.getElementById('input_loan_duration_').value="25";
+	} else if (duration<=0){
+		document.getElementById('input_loan_duration_').value="1";
+	}
+
+};
+
+
+index.regozoo=function(){
+	console.log("fct_internet");
+	var half = 1/2;
+ 
+	/*
+	 * On détermine le séparateur décimal du système (O.S.) de l'utilisateur
+	 * et on impose la valeur de dec et d'alpha en fonction des besoins.
+	 * Si le résultat contient une virgule le séparateur
+	 * décimal est la virgule sinon le point.
+	 */
+	var dec = (half.toString().match(/,/)) ? ',' : '.';
+ 
+	// Par opposition, on détermine celui qui ne l'est pas.
+	var alpha = (dec == '.') ? ',' : '.';
+ 
+	/*
+	 * On teste, au moment de la frappe, chaque caractère saisi
+	 * par l'utilisateur et on apporte les corrections nécessaires.
+	*/ 
+	$('.rate').keyup(function(){
+		console.log("on push");
+		var field = $(this);
+		var valNum = field.val();
+ 
+		/*
+		 * On remplace le non séparateur par le séparateur.
+		 * Si la chaîne commence par le séparateur, on met un zéro devant.
+		 */ 
+		valNum = valNum.replace(new RegExp("["+alpha+"]"), dec);
+		valNum = valNum.replace(new RegExp("^["+dec+"]"), '0.');
+ 
+		if (valNum != ''){
+			/*
+			 * Si la chaîne est au format monétaire (9999.99)
+			 * alors on ne change rien
+			 * sinon on teste si la chaîne comporte d'autres caractères que de 0 
+			 * à 9 ou le séparateur
+			 * alors, s'il la chaîne comporte d'autres caractères, on les supprime
+			 * sinon on limite le nombre de décimales à 2
+			 */ 
+			valNum = (new RegExp("^[0-9]+("+dec+"[0-9]{,2})?$").test(valNum))
+			       ? valNum 
+			       : (valNum.match(new RegExp("[^0-9"+dec+"]"))) 
+				    ? valNum.replace(new RegExp("[^0-9"+dec+"]","g"),'') 
+				    : valNum.match(new RegExp("^[0-9]+(["+dec+"][0-9]{0,2})?"))[0];
+		}
+ 
+		field.val(valNum);
+	});
+ 
+	// On s'assure du format de la saisie en fixant le nombre de décimales à 2
+	$('.rate').blur(function(){
+
+		var rate_loan= parseInt(document.getElementById("input_interest_rate_").value);
+			if(rate_loan>100){
+				document.getElementById('input_interest_rate_').value="100";
+			} else if (rate_loan<=0){
+				document.getElementById('input_interest_rate_').value="0";
+			}
+
+		var field = $(this);
+		var valNum = (field.val() * 1).toFixed(2);
+
+		
+ 
+		field.val(valNum);
+	});
+
+	
 };
 
 
