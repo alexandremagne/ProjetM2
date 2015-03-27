79ec780d9ca975ddca7578a1260688bdb148e30d
romaincucu <romain.cunault@gmail.com>
2015-03-27 18:57:32 +0100

    compteur

--- js/2_demande_pret.js
+++ js/2_demande_pret.js
@@ -231,7 +231,104 @@ obj.fun_affichage_spider=function(){
             data: [obj.affichage_spider.character, obj.affichage_spider.collateral, obj.affichage_spider.capacity, obj.affichage_spider.condition, obj.affichage_spider.capital]
         }]
     });//////container 2
+$('#containerGraph3').highcharts({
 
+        chart: {
+            type: 'gauge',
+            plotBackgroundColor: null,
+            plotBackgroundImage: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+
+        title: {
+            text: 'Your Score'
+        },
+
+        pane: {
+            startAngle: -150,
+            endAngle: 150,
+            background: [{
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                        [0, '#FFF'],
+                        [1, '#333']
+                    ]
+                },
+                borderWidth: 0,
+                outerRadius: '109%'
+            }, {
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                        [0, '#333'],
+                        [1, '#FFF']
+                    ]
+                },
+                borderWidth: 1,
+                outerRadius: '107%'
+            }, {
+                // default background
+            }, {
+                backgroundColor: '#DDD',
+                borderWidth: 0,
+                outerRadius: '105%',
+                innerRadius: '103%'
+            }]
+        },
+
+        // the value axis
+        yAxis: {
+            min: 0,
+            max: 100,
+
+            minorTickInterval: 'auto',
+            minorTickWidth: 1,
+            minorTickLength: 10,
+            minorTickPosition: 'inside',
+            minorTickColor: '#666',
+
+            tickPixelInterval: 30,
+            tickWidth: 2,
+            tickPosition: 'inside',
+            tickLength: 10,
+            tickColor: '#666',
+            labels: {
+                step: 2,
+                rotation: 'auto'
+            },
+            title: {
+                text: 'Success'
+            },
+            plotBands: [{
+                from: 0,
+                to: 40,
+                color: '#DF5353' // green 
+            }, {
+                from: 40,
+                to: 60,
+                color: '#DDDF0D' // yellow
+            }, {
+                from: 60,
+                to: 100,
+                color: '#55BF3B' // red DF5353
+            }]
+        },
+
+        series: [{
+            name: 'Your Score',
+            data: [parseInt(100-obj.affichage_spider.default_probability*100)],
+            tooltip: {
+                valueSuffix: ' %'
+            }
+        }]
+
+    },
+        // Add some life
+        function (chart) {
+           
+        });
  
 
 }
